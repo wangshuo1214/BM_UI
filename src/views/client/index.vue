711220c519b76857ed75d8a30d19eb11bd9cb22b
wangshuo02 <wangshuo02@css.com.cn>
2023-02-01 17:29:34 +0800

    客户关联修改

--- src/views/client/index.vue
+++ src/views/client/index.vue
@@ -28,22 +28,6 @@
           style="width: 200px"
         />
       </el-form-item>
-      <el-form-item label="客户类型" prop="item.clientType">
-        <el-select
-          v-model="queryParams.item.clientType"
-          placeholder="请选择客户类型"
-          clearable
-          size="small"
-          style="width: 150px"
-        >
-          <el-option
-            v-for="dict in clientTypeOptions"
-            :key="dict.dictCode"
-            :label="dict.dictName"
-            :value="dict.dictCode"
-          />
-        </el-select>
-      </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
@@ -74,9 +58,9 @@
     <el-table v-loading="loading" :data="clientList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="50" align="center" />
       <el-table-column key="clientName" label="客户名称" align="center" prop="clientName" :show-overflow-tooltip="true" />
-      <el-table-column key="clientType" label="客户类型" align="center" prop="clientType" :formatter="clientTypeFormat" />
       <el-table-column key="phone" label="手机号" align="center" prop="phone" :show-overflow-tooltip="true" />
       <el-table-column key="address" label="地址" align="center" prop="address" :show-overflow-tooltip="true" />
+      <el-table-column key="remark" label="备注" align="center" prop="remark" :show-overflow-tooltip="true" />
       <el-table-column label="修改时间" align="center" prop="updateDate" width="160">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.updateDate) }}</span>
@@ -119,19 +103,6 @@
           <el-col :span="24">
             <el-form-item label="客户名称" prop="clientName">
               <el-input v-model="form.clientName" placeholder="请输入客户名称" maxlength="30" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="客户类型" prop="clientType">
-              <el-radio-group v-model="form.clientType">
-                <el-radio
-                  v-for="dict in clientTypeOptions"
-                  :key="dict.dictCode"
-                  :label="dict.dictCode"
-                >{{ dict.dictName }}</el-radio>
-              </el-radio-group>
             </el-form-item>
           </el-col>
         </el-row>
@@ -191,8 +162,6 @@ export default {
       loading: true,
       // 客户表格数据
       clientList: [],
-      // 客户类型字典
-      clientTypeOptions: [],
       // 总条数
       total: 0,
       // 查询条件
@@ -205,7 +174,6 @@ export default {
         },
         item: {
           clientName: undefined,
-          clientType: undefined,
           phone: undefined,
           address: undefined
         }
@@ -221,9 +189,6 @@ export default {
   },
   created() {
     this.getList()
-    this.getDicts('client_type').then(response => {
-      this.clientTypeOptions = response.data
-    })
   },
   methods: {
     /** 查询客户列表 */
@@ -264,10 +229,6 @@ export default {
       this.resetForm('queryForm')
       this.handleQuery()
     },
-    // 客户类型字典翻译
-    clientTypeFormat(row, column) {
-      return this.selectDictName(this.clientTypeOptions, row.clientType)
-    },
     /** 新增按钮操作 */
     handleAdd() {
       this.reset()
@@ -278,7 +239,6 @@ export default {
     reset() {
       this.form = {
         clientName: undefined,
-        clientType: '0',
         phone: undefined,
         address: undefined,
         remark: undefined
