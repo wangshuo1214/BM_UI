c576d15e2a1b66297085463dc43c4cd7949f097e
wangshuo02 <wangshuo02@css.com.cn>
2023-04-13 17:48:37 +0800

    首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,30 +1,90 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }} </div>
+  <div class="layout">
+    <el-row>
+      <el-col :span="8">
+        <el-card shadow="hover" class="box-card" style="margin-left: 15px;margin-top: 10px;margin-bottom: 10px;margin-right: 10px;">
+          <div>
+            <span class="spanTitle">今日销售</span>
+          </div>
+          <div v-for="o in 1" :key="o" class="text item">
+            {{ '元 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="hover" class="box-card" style="margin-top: 10px;margin-bottom: 10px;">
+          <div>
+            <span class="spanTitle">本月销售</span>
+          </div>
+          <div v-for="o in 1" :key="o" class="text item">
+            {{ '元 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="hover" class="box-card" style="margin-left: 10px;margin-top: 10px;margin-bottom: 10px;margin-right: 15px;">
+          <div>
+            <span class="spanTitle">本年销售</span>
+          </div>
+          <div v-for="o in 1" :key="o" class="text item">
+            {{ '元 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8">
+        <el-card shadow="hover" class="box-card" style="margin-left: 15px;margin-bottom: 10px;margin-right: 10px;">
+          <div>
+            <span class="spanTitle">今日支出</span>
+          </div>
+          <div v-for="o in 1" :key="o" class="text item">
+            {{ '元 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="hover" class="box-card" style="margin-bottom: 10px;">
+          <div>
+            <span class="spanTitle">本月支出</span>
+          </div>
+          <div v-for="o in 1" :key="o" class="text item">
+            {{ '元 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="hover" class="box-card" style="margin-left: 10px;margin-bottom: 10px;;margin-right: 15px;">
+          <div>
+            <span class="spanTitle">本年支出</span>
+          </div>
+          <div v-for="o in 1" :key="o" class="text item">
+            {{ '元 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Home',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
+  name: 'Home'
 }
 </script>
 
-<style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+<style>
+  .spanTitle{
+    color: rgba(0,0,0,.45);
+    font-size: 14px;
+    line-height: 22px;
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
+  .text {
+    font-size: 14px;
   }
-}
+
+  .item {
+    margin-bottom: 18px;
+  }
 </style>
